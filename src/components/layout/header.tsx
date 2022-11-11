0fa3009ba2749a6e7327f3d08b02c2f2d3beee72
debugme <debugme@hotmail.com>
2022-11-11 10:22:10 +0000

    nineteenth commit

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
+import Logo from "../../images/logo.png"
+
 export const Header = () => {
   return (
-    <header className="bg-slate-700 drop-shadow-2xl flex justify-between px-4">
+    <header className="bg-slate-700 drop-shadow-2xl flex px-4">
+      <section className="flex justify-center items-center">
+        <Link  to="/">
+          <img className="w-9" src={Logo} alt="logo" />
+        </Link>
+        <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pt-2 tracking-[2px] ml-3 h-100 text-sm uppercase">RetroMatic</p>
+      </section>
     </header >
   )
 }
+
+

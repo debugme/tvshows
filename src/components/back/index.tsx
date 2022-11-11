f74b8da37894e712e93060bc51f0b17449284e9e
debugme <debugme@hotmail.com>
2022-11-11 11:12:15 +0000

    twentysecondth commit

--- src/components/back/index.tsx
+++ src/components/back/index.tsx
@@ -5,9 +5,8 @@ export const Back = () => {
   return (
     <nav className="col-span-1">
       <Link to="/">
-        <div className=" px-3 bg-slate-400 py-2 rounded-md w-[100px] h-12 relative border border-slate-500 hover:border-slate-900">
+        <div className="border relative h-12 w-12 rounded-full border-slate-500 hover:border-slate-600">
           <ArrowLeftIcon className="absolute top-[12px] left-[10px] h-6 inline-block text-slate-600" />
-          <p className="absolute top-[10px] left-[42px] text-lg text-slate-600">Back</p>
         </div>
       </Link>
     </nav>

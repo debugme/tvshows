4a51cb24d0c9455936f4d59f42e9b2070ac0687f
debugme <debugme@hotmail.com>
2022-11-12 19:10:39 +0000

    fortyfirst commit

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -13,7 +13,7 @@ export const Message: FC<MessageProps> = (props) => {
       <div className="grid grid-cols-message w-full">
         <div className="pr-4 flex flex-col justify-center items-end text-slate-600">
           <h5 className="text-xs md:text-base lg:text-lg uppercase">{subtitle}</h5>
-          <h3 className="text-xl md:text-4xl lg:text-5xl">{title}</h3>
+          <h3 className="text-2xl md:text-4xl lg:text-5xl">{title}</h3>
         </div>
         <div className="">{children}</div>
       </div>

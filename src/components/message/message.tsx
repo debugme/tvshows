2a1e5d9c3a7e33ebcf0820b664c5b89e4b880733
debugme <debugme@hotmail.com>
2022-11-13 11:12:11 +0000

    fortysecond commit

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -9,7 +9,7 @@ export const Message: FC<MessageProps> = (props) => {
   const { title, subtitle, children } = props
 
   return (
-    <section className="p-8 md:p-12 lg:p-20 mt-10 bg-slate-400 rounded-xl min-h-[220px] grid place-content-center">
+    <section className="p-8 md:p-12 lg:p-20 mt-10 bg-slate-400 rounded-xl h-[330px] grid place-content-center">
       <div className="grid grid-cols-message w-full">
         <div className="pr-4 flex flex-col justify-center items-end text-slate-600">
           <h5 className="text-xs md:text-base lg:text-lg uppercase">{subtitle}</h5>

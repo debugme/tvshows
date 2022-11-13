d7f0bbd9f28d9ca40cb61a80e45a05901229688c
debugme <debugme@hotmail.com>
2022-11-13 23:43:15 +0000

    fortyfour

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom"
 import { Bars3BottomRightIcon } from "@heroicons/react/24/outline"
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-import Logo from "./logo.png"
+import { Film } from "./film"
 
 export const Header = () => {
   return (
     <header className="bg-slate-700 drop-shadow-2xl flex justify-between px-4">
       <section className="flex justify-start items-center w-32">
         <Link className="flex relative" to="/">
-          <LazyLoadImage effect="blur" className="w-9" src={Logo} loading="lazy" alt="logo" width={36} height={36} />
-          <p className="absolute bottom-[4px] left-11 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pt-2 tracking-[2px] text-sm uppercase">showz</p>
+          <Film className="w-9 opacity-80 absolute bottom-[-22px]" />
+          <p className="absolute bottom-[-13px] left-11 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pt-2 tracking-[2px] text-sm uppercase">showz</p>
         </Link>
       </section>
 

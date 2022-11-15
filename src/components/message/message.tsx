419b49a043933ce8619b98d9994112200848de83
debugme <debugme@hotmail.com>
2022-11-15 12:19:31 +0000

    fiftythree

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -9,7 +9,7 @@ export const Message: FC<MessageProps> = (props) => {
   const { title, subtitle, children } = props
 
   return (
-    <section className="p-8 md:p-12 lg:p-20 mt-10 bg-slate-400 rounded-xl h-[330px] grid place-content-center">
+    <section className="p-8 md:p-12 lg:p-20 mt-10 bg-slate-400 rounded-xl h-[330px] grid place-content-center cursor-pointer">
       <div className="flex flex-wrap">
         <div className="max-h-[200px]">{children}</div>
         <div className="pr-4 text-slate-600">

0c05777e82ab2b58751a46842d268e76e10f71d0
debugme <debugme@hotmail.com>
2022-11-16 00:49:20 +0000

    sixtyone

--- src/components/search/popular.tsx
+++ src/components/search/popular.tsx
@@ -15,11 +15,11 @@ export const Popular: FC<PopularProps> = (props) => {
   return (
     <Fragment>
       <h2 className="text-3xl text-slate-600 w-20 mt-8">Popular</h2>
-      <dl className="flex gap-6 items-start pt-1">
-        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer" onClick={onClick}>The Flash</dd>
-        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer" onClick={onClick}>Supergirl</dd>
-        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer" onClick={onClick}>Batman</dd>
-        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer" onClick={onClick}>Gentleman Jack</dd>
+      <dl className="flex justify-start items-center pt-1 flex-wrap gap-0 md:gap-3 md:pb-2">
+        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer w-full md:w-auto text-center" onClick={onClick}>The Flash</dd>
+        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer w-full md:w-auto text-center" onClick={onClick}>Supergirl</dd>
+        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer w-full md:w-auto text-center" onClick={onClick}>Batman</dd>
+        <dd className="mt-3 py-2 px-3 bg-yellow-400 text-yellow-800 inline-block rounded-lg cursor-pointer w-full md:w-auto text-center" onClick={onClick}>Gentleman Jack</dd>
       </dl>
     </Fragment>
   )

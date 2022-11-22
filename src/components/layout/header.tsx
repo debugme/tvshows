ae7af1ce34c391265e5114d781cdec6a38ea7c2e
debugme <debugme@hotmail.com>
2022-11-22 13:18:27 +0000

    seventyseven

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom"
+
 import { Film, Menu } from "../images"
 
 export const Header = () => {
   return (
     <header className="flex justify-between items-center px-4 bg-header-bg font-cursive">
-      <span className="flex gap-2 relative">
-        <Film className="w-12 opacity-90" />
-        <h1 className="opacity-90 absolute top-[2px] left-[48px] -rotate-12 text-header-fg text-2xl bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-amber-500 to-yellow-500">Showz</h1>
-      </span>
+      <Link to="/">
+        <span className="flex gap-2 relative">
+          <Film className="w-12 opacity-90" />
+          <h1 className="opacity-90 absolute top-[2px] left-[48px] -rotate-12 text-header-fg text-2xl bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-amber-500 to-yellow-500">Showz</h1>
+
+        </span>
+      </Link>
       <Menu className="h-9 text-header-fg" />
     </header>
   )

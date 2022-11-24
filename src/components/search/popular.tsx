b1cb3066e56b714b89344746deeb03ff2a2cc400
debugme <debugme@hotmail.com>
2022-11-24 15:35:21 +0000

    eightynine

--- src/components/search/popular.tsx
+++ src/components/search/popular.tsx
@@ -2,8 +2,6 @@ import { FC, Fragment, MouseEventHandler } from "react"
 
 const popularTitles = ["The Flash", "Supergirl", "Batman", "Wonder Woman", "Spiderman", "X-Men"]
 
-const className = "mt-3 py-2 px-3 bg-primary-400 text-primary-800 inline-block rounded-lg cursor-pointer w-auto text-center"
-
 export type PopularProps = {
   setSearchTerm: (searchTerm: string) => void
   resultCount: number
@@ -18,7 +16,7 @@ export const Popular: FC<PopularProps> = (props) => {
   }
 
   const tagList = popularTitles.map(title => (
-    <dd className={className} key={title} onClick={onClick}>{title}</dd>
+    <dd className="mt-3 py-2 px-3 text-primary-800 bg-primary-300 inline-block rounded-lg cursor-pointer w-auto text-center" key={title} onClick={onClick}>{title}</dd>
   ))
 
   return (
